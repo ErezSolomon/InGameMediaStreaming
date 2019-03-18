cefcc4ea730748e02ebfc7cc57d01138341160b5
NetanelGin <48492102+NetanelGin@users.noreply.github.com>
2019-03-18 10:47:40 +0200

    Add files via upload

---
+++ Assets/UnityMediaStreaming/Scenes/Other/SpinAndDye.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinAndDye : MonoBehaviour
+{
+    public Transform Transform;
+    public Renderer rend;
+
+    void Start()
+    {
+        rend=GetComponent<Renderer>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Transform.Rotate(Vector3.up);
+        float lerp = Mathf.PingPong(Time.time, 3f);
+
+        rend.material.color = Color.Lerp(Color.red, Color.blue, lerp);
+    }
+}
---
+++ Assets/UnityMediaStreaming/Streaming/Scripts/CaptureScreen.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityMediaStreaming
+{
+    /// <summary>
+    /// If SourceTexture isn't null, capture the source texture.
+    /// Otherwise, if it's a component of a camera, capture camera at OnPostRender().
+    /// Otherwise, capture final entire game screen at OnGUI().
+    /// </summary>
+    [ExecuteInEditMode]
+    public class CaptureScreen : MonoBehaviour
+    {
+        public RenderTexture SourceTexture = null;
+
+        public delegate void CaptureFunc(Texture2D texture);
+        public delegate void TextureOutputFunc(CaptureFunc captureFunc);
+
+        public event TextureOutputFunc OnCapturingTime;
+
+        private bool hasCamera;
+
+        // Use this for initialization
+        void Start()
+        {
+            hasCamera = (GetComponent<Camera>() != null);
+        }
+
+        // Update is called once per frame
+        /*void Update () {
+
+        }*/
+
+        public int Width { get { return (SourceTexture == null) ? Screen.width : SourceTexture.width; } }
+        public int Height { get { return (SourceTexture == null) ? Screen.height : SourceTexture.height; } }
+
+        private void Capture(Texture2D textureToDraw)
+        {
+            /*if (OutputStream == null || OutputStream.TextureToDraw == null || !OutputStream.ShouldCapture())
+                return;
+            // otherwise
+            */
+
+            if (textureToDraw == null)
+                return;
+            // otherwise
+
+            int width = this.Width;
+            int height = this.Height;
+
+            if (textureToDraw.width != width || textureToDraw.height != height)
+                textureToDraw.Resize(width, height);
+
+            RenderTexture.active = SourceTexture;
+
+            textureToDraw.ReadPixels(new Rect(0, 0, width, height),
+                0, 0, false);
+            textureToDraw.Apply();
+
+            //OutputStream.Process();
+        }
+
+        private void CallOnCapturingTime()
+        {
+            if (OnCapturingTime != null)
+                OnCapturingTime(Capture);
+        }
+
+        private void OnPostRender()
+        {
+            if (hasCamera)
+                CallOnCapturingTime();
+        }
+
+        private void OnGUI()
+        {
+            if (!hasCamera)
+                CallOnCapturingTime();
+        }
+    }
+}
